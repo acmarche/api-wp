c2fd164626c397f22b5758b7d97d84615ff2a6a6
jfsenechal <jfsenechal@gmail.com>
2021-10-27 14:37:09 +0200

    create post and update ok

--- src/ApiClient.php
+++ src/ApiClient.php
@@ -17,8 +17,7 @@ class ApiClient
     {
         Env::loadEnv();
         $this->url = get_site_url().'/wp-json/wp/v2';
-        dump($this->url);
-        $options = new HttpOptions();
+        $options   = new HttpOptions();
         $options->setAuthBasic($_ENV['WP_USER'], $_ENV['WP_PASSWORD']);
         $this->httpClient = HttpClient::createForBaseUri($this->url, $options->toArray());
     }
@@ -39,16 +38,40 @@ class ApiClient
      * @throws \Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface
      * @throws \Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface
      */
-    public function post(array $data)
+    public function createPost(array $data, ?int $postId = null)
     {
-        $response = $this->httpClient->request('POST', $this->url.'/posts', [
+        $url = $this->url.'/posts';
+        if ($postId) {
+            $url .= '/'.$postId;
+        }
+        $response = $this->httpClient->request('POST', $url, [
             'body' => $data,
         ]);
         $httpLogs = $response->getInfo('response_headers');
         dump($httpLogs);
         $statut = $response->getStatusCode();
         dump($statut);
-        $content  = $response->getContent();
+        $content = $response->getContent();
+
+        dump($content);
+
+
+    }
+
+    public function createAttachement(array $data, ?int $postId = null)
+    {
+        $url = $this->url.'/media';
+        if ($postId) {
+            $url .= '/'.$postId;
+        }
+        $response = $this->httpClient->request('POST', $url, [
+            'body' => $data,
+        ]);
+        $httpLogs = $response->getInfo('response_headers');
+        dump($httpLogs);
+        $statut = $response->getStatusCode();
+        dump($statut);
+        $content = $response->getContent();
 
         dump($content);
 
